edc6e52c1968fe51a43053c2ea544675304700d0
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-28 12:43:19 +0530

    Update sit_sg.tf

--- sit_sg.tf
+++ sit_sg.tf
@@ -1,1 +1,24 @@
+resource "aws_security_group" "sit_sg"{
+  name = "sit_sg"
+  ingress{
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags{
+    Name = "sit_sg"
+  }
+  
+}
 
+output "sit_sg_id"{
+  value = aws_security_group.sit_sg.id
+}
