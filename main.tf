f535f6e1db08617c8ebc1049e3d779d0e9395dda
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-28 12:54:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
-
+resource "aws_security_group" "sg"{
+  name = var.name
+  ingress{
+    from_port = var.from_port
+    to_port = var.to_port
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags{
+    Name = var.name
+  }
+  
+}
+output "sg_id"{
+  value = aws_security_group.sg.id
+}
