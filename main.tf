5e94a383a69facc1b0fd00ea7244cb352658fb1a
Shantanusri123 <105036664+Shantanusri123@users.noreply.github.com>
2022-07-29 16:20:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+data "terraform_remote_state" "security_group_info"{
+  backend = "s3"
+  config = {
+    bucket               = "terraform-state-for-us"
+    key                  = var.environment_key
+    region  = var.aws_region
+    profile = "aws_profile_dev"
+  }
+}
+
 resource "aws_security_group" "sg"{
   name = var.name
   ingress{
